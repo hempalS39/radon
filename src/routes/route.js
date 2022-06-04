12cf6b8819a9849af6a698e290e5ecd3478c18ec
hempalS39 <hs527196@gmail.com>
2022-06-04 13:48:47 +0530

    get-api-moviesAssignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,81 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+
+// <------------------problem-1- GET/movies api--------------------------->
+
+router.get("/movies", function(req, res) {
+    let moviesArr = ["Matrix", "Puspa", "Avengers", "Suryavansum", "KGF"]
+    
+    res.send(  { msg: moviesArr }  )
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
+// <------------------problem-2 && 3- GET/movies/indexNUmber-api------------------------->
+//Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1). You can define an array of movies again in your api
+
+router.get("/movies/:indexNumber", function (req, res){
+    let moviesArr = ["Matrix", "Pushpa", "Avenger", "Suryanvasum", "KGF"]
+    let index = req.params.indexNumber
+    
+    if(index<moviesArr.length){ 
+    let movie = moviesArr[index]
+    res.send({movie: movie})
+    }
+    else{
+        res.send({msg: "indexNumber is invalid, use a valid index Number"})
+    }
+})
+
+// <------------------problem-4-GET/films-api------------------------->
+//Write another api called GET /films. Instead of an array of strings define an array of movie objects this time. Each movie object should have values - id, name.
+//Return the entire array in this api’s response
+
+ let arrObj = [
+    {'id': 1, 
+      'name': 'Avenger' },
+    {
+        'id': 2,
+        'name':'Pushpa'
+       }, {
+        'id': 3,
+        'name': 'Matrix'
+       }, {
+        'id': 4,
+        'name': 'KGF'
+       },
+       {
+        'id': 5,
+        'name': 'Sherdil-The Pilibhit Saga'
+       }
+           
+]
+router.get("/films", function(req, res){
+    
+    res.send({arrayOfObj: arrObj})
+})
+
+// <------------------problem-5-GET/films/:filmId-api------------------------->
+//Write api GET /films/:filmId where filmId is the value received in request path params. Use this value to return a movie object with this id. In case there is no such movie present in the array, return a suitable message in the response body. Example for a request GET /films/3 should return the movie object 
+// {
+//     “id”: 3,
+//     “name”: “Rang de Basanti”
+//    }
+//    Similarly for a request GET /films/9 the response can be something like - ‘No movie exists with this id’
+
+router.get("/films/:filmId" , function (req , res){
+    let id = (req.params.filmId)
+    // console.log(`id=${id} and id-1=${id-1}`)
+    if(id>0 && id<=arrObj.length){
+        
+        res.send(arrObj[id-1]);
+    }
+     else{
+         res.send({msg: "No movie exists with this id"
+         })
+     }
+       
 })
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
 module.exports = router;
